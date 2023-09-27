66c1352508ba4e7f3846afc992cbd28225ad540f
Dan Merkin <dan.merkin@gmail.com>
2023-09-27 18:16:42 -0400

    Adding force:false to ensure tables are not dropped

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync().then(() => {
+// Force: false to ensure the tables are not dropped when starting the server
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
